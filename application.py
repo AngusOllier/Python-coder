27c23f0940fce5ef35177524de063ab878d2bd66
John Ollier <john.ollier@landregistry.gov.uk>
2019-03-07 21:44:31 +0000

    Move transform to web application
    Handle form submission

--- application.py
+++ application.py
@@ -1,11 +1,33 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 @app.route('/codec')
-def hello(name=None):
+def codec():
     return render_template('decoder.html')
+
+
+@app.route('/transform', methods=['POST'])
+def do_codec():
+    action = 'encode'
+    transformed = transform(request.form['message'], action)
+    return render_template('decoder.html', action=action, transformed=transformed)
+
+
+def transform(message, action, key=3):
+    newMessage = ''
+    for character in message:
+        position = alphabet.find(character)
+        if 'encode' == action:
+            new_position = (position + key) % 52
+        else:
+            new_position = (position - key) % 52
+        new_character = alphabet[new_position]
+        newMessage += new_character
+        newMessage += character
+    return newMessage
 
 
 if __name__ == "__main__":
